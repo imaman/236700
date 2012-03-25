480258c5d4ca218a3ccb1f131550670324d0a097
Itay Maman <itay.maman@gmail.com>
2012-03-25 22:08:10 +0200

    remove location details

--- src/shapes/Shapes.java
+++ src/shapes/Shapes.java
@@ -1,39 +1,32 @@
 package shapes;
 
-import java.awt.geom.Point2D;
 
 public class Shapes {
   
   public static class Triangle {
-    public final Point2D vertexA;
-    public final Point2D vertexB;
-    public final Point2D vertexC;
+    public final double base;
+    public final double height;
     
-    public Triangle(Point2D vertexA, Point2D vertexB, Point2D vertexC) {
-      this.vertexA = vertexA;
-      this.vertexB = vertexB;
-      this.vertexC = vertexC;
+    public Triangle(double base, double height) {
+      this.base = base;
+      this.height = height;
     }
   }
 
   public static class Rectangle {
-    public final Point2D center;
     public final double width;
     public final double height;
     
-    public Rectangle(Point2D center, double width, double height) {
-      this.center = center;
+    public Rectangle(double width, double height) {
       this.width = width;
       this.height = height;
     }
   }
 
   public static class Circle {
-    public final Point2D center;
     public final double radius;
     
-    public Circle(Point2D center, double radius) {
-      this.center = center;
+    public Circle(double radius) {
       this.radius = radius;
     }
   }
