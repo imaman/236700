af8dd8785cbd30a63ed7f77a32dfe5a29f590de6
Itay Maman <itay.maman@gmail.com>
2012-03-26 00:42:43 +0200

    inline everything into watchdog - now a big-ball-of-mud

--- src/watchdog/Watchdog.java
+++ src/watchdog/Watchdog.java
@@ -6,81 +6,30 @@ import java.util.Scanner;
 
 public class Watchdog {
 
-  public static class Fetcher {
-    String fetch(String address) throws Exception {
-      URL url = new URL(address);
-      StringBuilder result = new StringBuilder();
-      for (Scanner sc = new Scanner(url.openStream()); sc.hasNext(); ) {
-        result.append(sc.nextLine()).append("\n");
-      }
-      
-      return result.toString();
-    }
+  private final boolean checkContent;
+  private final int minLength;
+
+  public Watchdog(boolean checkContent, int minLength) {
+    this.checkContent = checkContent;
+    this.minLength = minLength;
   }
-  
-  public static class Alerter {  
-    
-    private Checker checker;
-    
-    void alert() {
+
+  public void check(String address) throws Exception {
+    URL url = new URL(address);
+    StringBuilder result = new StringBuilder();
+    for (Scanner sc = new Scanner(url.openStream()); sc.hasNext(); ) {
+      result.append(sc.nextLine()).append("\n");
+    }
+    String html = result.toString();
+    if ((minLength >= 0) && html.length() < minLength || 
+        checkContent && !html.contains("body")) { 
       Date now = new Date();
       System.out.println("Alert fired at " + now + " - please check!");
     }
-
-    public void check(String address) throws Exception {
-      if (!checker.check(address)) {
-        alert();
-      }
-    }
-
-    public void setConfig(boolean checkContent, int minLength) {
-      checker = new Checker(checkContent, minLength >= 0);
-      checker.setMinLength(minLength);
-    }
-  }
-  
-  public static class Checker {
-
-    private int minLength;
-    private boolean checkContent;
-    private boolean checkLength;
-    private final Fetcher fetcher = new Fetcher();
-
-
-    public Checker(boolean checkContent, boolean checkLength) {
-      this.checkContent = checkContent;
-      this.checkLength = checkLength;
-    }
-
-    public void setMinLength(int newValue) {
-      this.minLength = newValue;
-    }
-
-    public boolean check(String address) throws Exception {
-      String html = fetcher.fetch(address);
-      if (checkLength && html.length() < minLength) {
-        return false;
-      }
-      if (checkContent && !html.contains("body")) {
-        return false;
-      }
-      return true;
-    }
   }
 
-  private final Alerter alerter;
-    
-  public Watchdog(Alerter alerter) {
-    this.alerter = alerter;
-  }
-  
-  public void run(String address) throws Exception {
-    alerter.check(address);
-  }
-  
   public static void main(String[] args) throws Exception {    
-    Alerter alerter = new Alerter();
-    alerter.setConfig(true, 5000);
-    new Watchdog(alerter).run("http://www.jquery.org");   
+    Watchdog watchdog = new Watchdog(true, 5000);
+    watchdog.check("http://www.jquery.org");   
   }
 }
