48adfc36bf9458e975dede4afdfe95c46c693644
Itay Maman <itay.maman@gmail.com>
2012-03-31 23:01:01 +0300

    extracted constants

--- src/allocations/Urls.java
+++ src/allocations/Urls.java
@@ -1,18 +1,27 @@
 package allocations;
 
+import static org.junit.Assert.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Urls {
 
+  private static final String TRANSACTIONS = "transactions/";
+  private static final String TARGET_TYPE_AND_ID = "/{targetType}/{targetId}/";
+  private static final String SOURCE_ID = "{sourceId}";
+  private static final String SOURCE_TYPE = "{sourceType}";
+  private static final String SOURCE_TYPE_AND_ID = SOURCE_TYPE + "/" + SOURCE_ID;
+  private static final String WHEN = "{year}/{month}";
+  
   public final String transactionsOnDate;
   public final String createTransaction;
   public final String deleteTransaction;
   
   public Urls(Map<String, Object> params) {
-    transactionsOnDate = populate("transactions/{sourceType}/on/{year}/{month}", params); 
-    createTransaction = populate("transactions/{sourceType}/{sourceId}/{targetType}/{targetId}/{year}/{month}/{percentage}", params);
-    deleteTransaction = populate("transactions/{sourceType}/{sourceId}/{targetType}/{targetId}/{year}/{month}", params);
+    transactionsOnDate = populate(TRANSACTIONS + SOURCE_TYPE + "/on/" + WHEN, params); 
+    createTransaction = populate(TRANSACTIONS + SOURCE_TYPE_AND_ID + TARGET_TYPE_AND_ID + WHEN + "/{percentage}", params);
+    deleteTransaction = populate(TRANSACTIONS + SOURCE_TYPE_AND_ID + TARGET_TYPE_AND_ID + WHEN, params);
   }
 
   private String populate(String template, Map<String, Object> params) {
@@ -46,8 +55,13 @@ public class Urls {
     map.put("percentage", "%%");
     
     Urls urls = new Urls(map);
+    
     System.out.println(urls.createTransaction);
     System.out.println(urls.deleteTransaction);
     System.out.println(urls.transactionsOnDate);
+
+    assertEquals("transactions/sT/sID/tT/tID/YY/MM/%%", urls.createTransaction);
+    assertEquals("transactions/sT/sID/tT/tID/YY/MM", urls.deleteTransaction);
+    assertEquals("transactions/sT/on/YY/MM", urls.transactionsOnDate);
   }
 }
