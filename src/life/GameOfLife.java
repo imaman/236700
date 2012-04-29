4444b5d6ad5090c5b9a2993f7af78984c6fc6e89
Itay Maman <itay.maman@gmail.com>
2012-04-29 14:05:12 +0300

    inlined step(int,int)

--- src/life/GameOfLife.java
+++ src/life/GameOfLife.java
@@ -23,9 +23,30 @@ public class GameOfLife {
   
   public void step() {
     Set<int[]> nextLivingCells = new HashSet<int[]>();
-    for (int c = 0; c < grid.length; ++c) {
-      for (int r = 0; r < grid[c].length; ++r) {
-        if (step(c, r)) 
+    for (int c = 0; c < columns; ++c) {
+      for (int r = 0; r < rows; ++r) {
+        int numLivingNeighbors = 0;
+        for (int i = c - 1; i <= c + 1; ++i) {
+          for (int j = r - 1; j <= r + 1; ++j) {
+            if (i == c && j == r)
+              continue;
+            
+            if ((i >= 0 && i < columns && j >= 0 && j < rows) 
+                  ? grid[i][j]
+                  : false) 
+              numLivingNeighbors += 1;
+          }
+        }
+        
+        boolean isLiveAtCurrentStage = 
+          (c >= 0 && c < columns && r >= 0 && r < rows) 
+          ? grid[c][r]
+          : false;
+          
+        boolean liveAtNextStage = isLiveAtCurrentStage 
+            ? numLivingNeighbors == 2 || numLivingNeighbors == 3
+            : numLivingNeighbors == 3; 
+        if (liveAtNextStage) 
           nextLivingCells.add(new int[] { c, r });
       }
     }
@@ -41,26 +62,6 @@ public class GameOfLife {
     }
   }
    
-  public boolean step(int col, int row) {
-    int numLivingNeighbors = 0;
-    for (int i = col - 1; i <= col + 1; ++i) {
-      for (int j = row - 1; j <= row + 1; ++j) {
-        if (i == col && j == row)
-          continue;
-        
-        if (isLive(i, j)) 
-          numLivingNeighbors += 1;
-      }
-    }
-    boolean isLive = (col >= 0 && col < columns && row >= 0 && row < rows) 
-      ? grid[col][row]
-      : false;
-      
-    return isLive 
-      ? numLivingNeighbors == 2 || numLivingNeighbors == 3
-      : numLivingNeighbors == 3;
-  }
-
   private boolean isLive(int col, int row) {
     return (col >= 0 && col < columns && row >= 0 && row < rows) 
       ? grid[col][row]
