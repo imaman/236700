753b24c3e0b2e8c6f45d09d386ace395c1211782
Itay Maman <itay.maman@gmail.com>
2012-06-24 19:43:55 +0300

    transition are now explicit - underlying system is more powerful, most of our programming is now in the configuration code

--- src/dsl/LoanRulesExplicitStates.java
+++ src/dsl/LoanRulesExplicitStates.java
@@ -13,97 +13,112 @@ public class LoanRulesExplicitStates {
   private static final int SUPER_NEGATIVE_BALANCE = -5000;
   private final List<Event> events = new ArrayList<Event>();
   
-  public abstract static class State {    
-    public abstract State run(Event e);
+  public static class Transition {
+    public final Condition condition;
+    public final State to;
     
-    private Map<String, State> nextStateByName = new HashMap<String, State>();
-    
-    void addTransition(String name, State to) {
-      nextStateByName.put(name, to);
+    public Transition(Condition condition, State to) {
+      this.condition = condition;
+      this.to = to;
     }
+  }
+  
+  public static class State {    
+    public void run(Event e) {}
+      
+    private List<Transition> transitions = new ArrayList<Transition>();
     
-    protected State transition(String transitionName) {
-      return nextStateByName.get(transitionName); 
+    public void addTransition(Condition condition, State to) {
+      transitions.add(new Transition(condition, to));
+    }
+
+    public State processEvent(Event e) {
+      for (Transition t : transitions)
+        if (t.condition.check(e))
+          return t.to;
+      return null;
     }
   }
 
-  State idle = new State() {
-
-    @Override
-    public State run(Event e) {
-      if (e.isPayment())
-        return transition("payment");
-      return null;
-    }
-  };
-
   
-  State active = new State() {
-    @Override
-    public State run(Event e) {
-      if (e.isDelayedPayment()) 
-        return transition("delyed-payment");
-      else if (e.isLastPayment())
-        return transition("last-payment");
-      else if (e.isPayment()) {
-        // Process the payment ...
-      }
-      
-      return null;
-    }
-  };
+  public abstract static class Condition {
+    public abstract boolean check(Event e);
+  }
   
+  State idle = new State(); 
+  State active = new State();
+  State waitForFine = new State();  
+  State waitForPositive = new State();
+
   State restricted = new State() {
     @Override
-    public State run(Event e) {        
-      if (e.isDelayedPayment())
-        return transition("delyed-payment");
-      else if (e.isBalanceEvent() && e.getBalance() < 0)
+    public void run(Event e) {        
+      if (e.isBalanceEvent() && e.getBalance() < 0)
         e.reject();
-      else if (e.isPayment())
-        return transition("payment");
-      
-      return null;
     }
   };
-  
-  State waitForFine = new State() {
+    
+  State superRestricted = new State() {
     @Override
-    public State run(Event e) {
-      if (e.isFineEvent())
-        return transition("fine-paid");
-      
-      return null;
+    public void run(Event e) {
+      if (e.isBalanceEvent() && e.getAmount() < 0)
+        e.reject();    
+    }
+  };
+    
+  State done = new State() {
+    @Override
+    public void run(Event e) {
+      System.out.println("-- loan returned --");
+    }
+  };  
+  
+  public static Condition isPayment = new Condition() {
+    @Override
+    public boolean check(Event e) {
+      return e.isPayment();
     }
   };
 
-  
-  State superRestricted = new State() {
+  public static Condition isDelayedPayment = new Condition() {
     @Override
-    public State run(Event e) {
-      if (e.isBalanceEvent() && e.getAmount() < 0)
-        e.reject();    
-      else if (e.isBalanceEvent() && e.getBalance() > SUPER_POSITIVE_BALANCE)
-        return transition("super-positive");
-      
-      return null;
+    public boolean check(Event e) {
+      return e.isDelayedPayment();
     }
   };
   
-  State waitForPositive = new State() {
+  public static Condition isLastPayment = new Condition() {
     @Override
-    public State run(Event e) {
-      if (e.isBalanceEvent() && e.getBalance() > 0)
-        return transition("positive");
-      return null;
+    public boolean check(Event e) {
+      return e.isLastPayment();
     }
   };
   
-  State done = new State() {
+  public static Condition isFinePaid = new Condition() {
     @Override
-    public State run(Event e) {
-      System.out.println("-- loan returned --");
-      return transition("back-to-idle");
+    public boolean check(Event e) {
+      return e.isFineEvent();
+    }
+  };
+
+  public static Condition isSuperPositiveBalance = new Condition() {
+    @Override
+    public boolean check(Event e) {
+      return e.isBalanceEvent() && e.getBalance() > SUPER_POSITIVE_BALANCE;
+    }
+  };
+
+  public static Condition isPositiveBalance = new Condition() {
+    @Override
+    public boolean check(Event e) {
+      return e.isBalanceEvent() && e.getBalance() > 0;
+    }
+  };
+
+  public static Condition always = new Condition() {
+    @Override
+    public boolean check(Event e) {
+      return true;
     }
   };
   
@@ -111,15 +126,15 @@ public class LoanRulesExplicitStates {
     // Overall structure of the machine is now well localized
     // (although each state is now less localized).
     
-    idle.addTransition("payment", active);
-    active.addTransition("delayed-payment", restricted);
-    active.addTransition("last-payment", waitForPositive);    
-    restricted.addTransition("delayed-payment", superRestricted);
-    restricted.addTransition("payment", active);    
-    waitForFine.addTransition("fine-paid", active);   
-    superRestricted.addTransition("super-positive", waitForFine);    
-    waitForPositive.addTransition("positive", done);
-    done.addTransition("back-to-idle", idle);
+    idle.addTransition(isPayment, active);
+    active.addTransition(isDelayedPayment, restricted);
+    active.addTransition(isLastPayment, waitForPositive);    
+    restricted.addTransition(isDelayedPayment, superRestricted);
+    restricted.addTransition(isPayment, active);    
+    waitForFine.addTransition(isFinePaid, active);   
+    superRestricted.addTransition(isSuperPositiveBalance, waitForFine);    
+    waitForPositive.addTransition(isPositiveBalance, done);
+    done.addTransition(always, idle);
   }
   
   public void run() {
@@ -128,8 +143,8 @@ public class LoanRulesExplicitStates {
       State next = null;
       if (e.isBalanceEvent() && e.getBalance() < SUPER_NEGATIVE_BALANCE)
         next = restricted; 
-      else 
-        next = state.run(e);
+      else  
+        next = state.processEvent(e);
       
       state = next == null ? state : next;
     }
@@ -147,8 +162,8 @@ public class LoanRulesExplicitStates {
       return;
     
     visited.add(state);
-    for (State s : state.nextStateByName.values())
-      traverse(s, visited);
+    for (Transition t : state.transitions) 
+      traverse(t.to, visited);
   }
   
   public static void main(String[] args) {
