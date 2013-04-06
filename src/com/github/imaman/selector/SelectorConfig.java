eff810f20c4a5af6c5b17688d6ae981d512e46aa
Itay Maman <itay.maman@gmail.com>
2013-04-06 12:44:10 +0300

    implement one side of the fake it - loading config data

--- src/com/github/imaman/selector/SelectorConfig.java
+++ src/com/github/imaman/selector/SelectorConfig.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.github.imaman.selector.external.Config;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -18,7 +20,18 @@ public class SelectorConfig extends Config {
 
   public Map<String, Integer> map() {
     Map<String, Integer> result = new HashMap<>();
-    result.put("G_G/A", 1);
+    JsonArray array = data.getAsJsonArray("generators");
+    for (JsonElement current : array) {
+      JsonObject obj = current.getAsJsonObject();
+      String name = obj.get("name").getAsString();
+      JsonArray labels = obj.getAsJsonArray("default_labels");
+      for (JsonElement label : labels) {
+        String labelName = label.getAsJsonArray().get(0).getAsString();
+        int revision = label.getAsJsonArray().get(1).getAsInt();
+
+        result.put(name + "/" + labelName, revision); // <-- DRY violation w.r.t labelId()
+      }
+    }
     return result;
   }
 
---
+++ src/com/github/imaman/selector/SelectorConfig_Test.java
@@ -0,0 +1,42 @@
+package com.github.imaman.selector;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SelectorConfig_Test {
+
+  @Test
+  public void translatesInputIntoLabelIdToRevisionMap() {
+    SelectorConfig config = new SelectorConfig();
+    config.LoadFromString("{" +
+        "generators: [" +
+        "  { name: 'G_G', default_labels: [['A', 100]] }]}");
+
+    assertEquals(100, config.map().get("G_G/A").intValue());
+  }
+
+  @Test
+  public void aGeneratorCanHaveSeveralDefaultLabels() {
+    SelectorConfig config = new SelectorConfig();
+    config.LoadFromString("{" +
+        "generators: [" +
+        "  { name: 'G_G', default_labels: [['A', 100], ['B', 200]] }]}");
+
+    assertEquals(100, config.map().get("G_G/A").intValue());
+    assertEquals(200, config.map().get("G_G/B").intValue());
+  }
+
+  @Test
+  public void canAccomodateMultipleGenerators() {
+    SelectorConfig config = new SelectorConfig();
+    config.LoadFromString("{" +
+        "generators: [" +
+        "  { name: 'First', default_labels: [['L1', 501]] }," +
+        "  { name: 'Second', default_labels: [['L2', 502]] }" +
+        "]}");
+
+    assertEquals(501, config.map().get("First/L1").intValue());
+    assertEquals(502, config.map().get("Second/L2").intValue());
+  }
+}
