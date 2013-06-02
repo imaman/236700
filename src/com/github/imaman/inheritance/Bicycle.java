e89b43cb8576adb3da1c356aaf87a17313bb1ad3
Itay Maman <imaman@google.com>
2013-06-02 22:15:54 +0300

    Mountain bike behavior implemented inside the Bicycle class

--- src/com/github/imaman/inheritance/Bicycle.java
+++ src/com/github/imaman/inheritance/Bicycle.java
@@ -1,11 +1,16 @@
 package com.github.imaman.inheritance;
 
+import java.util.Objects;
+
 import com.google.gson.JsonObject;
 
 public class Bicycle {
 
   private String size;
   private String tapeColor;
+  private String style;
+  private String frontShock;
+  private String rearShock;
   
   public Bicycle(String json) {
     this(new Record(json));
@@ -14,13 +19,22 @@ public class Bicycle {
   public Bicycle(Record record) {
     this.size = record.get("size");
     this.tapeColor = record.get("tape_color");
+    this.style = record.get("style");
+    this.frontShock = record.get("front_shock");
+    this.rearShock = record.get("rear_shock");
   }
     
   public Record spares() {
     JsonObject json = new JsonObject();
-    json.addProperty("chain", "10-speed");
-    json.addProperty("tire_size", "23");
-    json.addProperty("tape_color", tapeColor);
+    if (!Objects.equals("Mountain", style)) {
+      json.addProperty("chain", "10-speed");
+      json.addProperty("tire_size", "23");
+      json.addProperty("tape_color", tapeColor);
+    } else {
+      json.addProperty("chain", "10-speed");
+      json.addProperty("tire_size", "2.1");
+      json.addProperty("rear_shock", rearShock);      
+    }
     return new Record(json);
   }
   
@@ -34,6 +48,10 @@ public class Bicycle {
   
   public boolean isTapeColor(String otherTapeColor) {
     return tapeColor.compareToIgnoreCase(otherTapeColor) == 0;
+  }
+  
+  public String getFrontShock() { 
+    return frontShock;
   }
   
   // additional method are here..
--- src/com/github/imaman/inheritance/Bicycle_Test.java
+++ src/com/github/imaman/inheritance/Bicycle_Test.java
@@ -21,10 +21,16 @@ public class Bicycle_Test {
     assertTrue(bike.isTapeColor("yellow"));
     assertFalse(bike.isTapeColor("red"));
   }
-
   @Test
   public void providesSpares() {
     Bicycle bike = new Bicycle("size: 'M', tape_color: 'red'");
     assertTrue(bike.spares().isEquivalentTo("tire_size: '23', chain: '10-speed', tape_color: 'red'"));
+  }
+  
+  @Test
+  public void canBeMountainBike() {
+    Bicycle bike = new Bicycle("style: 'Mountain', size: 'S', front_shock: 'Manitou', rear_shock: 'Fox'");
+    assertTrue("spares=" + bike.spares(), 
+        bike.spares().isEquivalentTo("tire_size: '2.1', chain: '10-speed', rear_shock: 'Fox'"));
   }
 }
