40ebf1dd9652778edb00083203589a7804959b0e
Itay Maman <imaman@google.com>
2013-06-03 00:14:57 +0300

    delegates are now constant objects

--- src/com/github/imaman/inheritance/RoadBike.java
+++ src/com/github/imaman/inheritance/RoadBike.java
@@ -6,17 +6,21 @@ public class RoadBike extends Bicycle {
 
   private String tapeColor;
 
+  private static CustomSpares TAPE_COLOR_SPARES = new CustomSpares() {      
+    @Override public void populate(Record record, Map<String, String> output) {
+      output.put("tape_color", record.get("tape_color"));
+    }
+  };
+  
+  private static TireSize ROAD_BIKE_TIRE_SIZE = new TireSize() {
+    @Override public String calculate() { return "23"; }
+  };
+  
   public RoadBike(final Record record) {
-    super(record, new TireSize() {
-      @Override public String calculate() { return "23"; }
-    }, new CustomSpares() {      
-      @Override public void populate(Record record, Map<String, String> output) {
-        output.put("tape_color", record.get("tape_color"));
-      }
-    });
+    super(record, ROAD_BIKE_TIRE_SIZE, TAPE_COLOR_SPARES);
     this.tapeColor = record.get("tape_color");
   }
-  
+
   public RoadBike(String json) {
     this(new Record(json));
   }
